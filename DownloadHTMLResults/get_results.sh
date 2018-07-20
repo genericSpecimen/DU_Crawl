a419b7e2a66288152a539b88a0d30734241e3390
genericSpecimen <irfan.sh3112@gmail.com>
2018-07-20 14:15:38 +0530

    fixed typo

--- DownloadHTMLResults/get_results.sh
+++ DownloadHTMLResults/get_results.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for i in {17058570001..17058570002}; do
+for i in {17058570001..17058570052}; do
 	echo "Welcome $i, please wait while I try to fetch your result."
 	scrapy runspider --nolog get_html.py -a roll_number=$i
 
